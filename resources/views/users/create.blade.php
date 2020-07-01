e106157306826fce1a88e54bc6f0f21d8337c70d
郑佳坛 <13060590076@163.com>
2020-07-01 15:15:53 +0800

    用户注册表单

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -4,5 +4,39 @@
 
 
 @section('content')
-<h1>注册</h1>
+<div class="offset-md-2 col-md-8">
+	<div class="card">
+		<div class="card-header">
+			<h5>注册</h5>
+		</div>
+		<div class="card-body">
+			<form action="{{ route('users.store') }}" method="POST">
+				<div class="form-group">
+					<label for="name">名称：</label>
+					<input type="text" class="form-control" name value="{{ old('name') }}">
+				</div>
+
+
+				<div class="form-group">
+					<label for="email">邮箱：</label>
+					<input type="text" class="form-control" name="email" value="{{ old('email') }}">
+				</div>
+
+
+				<div class="form-group">
+					<label for="password">密码：</label>
+					<input type="password" class="form-control" name="password" value="{{ old('password') }}">
+				</div>
+
+
+				<div class="form-group">
+					<label for="password_confirmation">确认密码：</label>
+					<input type="password" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}">
+				</div>
+
+				<button type="submit" class="btn btn-primary">注册</button>
+			</form>
+		</div>
+	</div>
+</div>
 @stop
